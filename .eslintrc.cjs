370e19b3963ba9b63e308e9d6439b213f979eada
Fabian Gutierrez <fega.hg@gmail.com>
2023-06-26 00:45:46 -0500

    chore(MAMORU-655): prevent .only in test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,18 @@
 module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    rules: {
-        'no-console': 'error',
-    },
+    plugins: ['@typescript-eslint','jest'],
     root: true,
+    env:{
+        node: true,
+        jest: true,
+    },
+    rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error",
+        'no-console': 'error'
+      },
 };
