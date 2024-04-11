f305f5486e742ec005174f0ab4137fce5a3859c8
Olexander Hofman <oh@mamoru.network>
2024-04-11 16:59:16 +0100

    feat: add cosmos sdk, update sdk versions

--- scripts/generate-dependency-map.sh
+++ scripts/generate-dependency-map.sh
@@ -6,12 +6,14 @@ SDK_VERSION=$(echo $PACKAGE_CONTENTS | jq --raw-output '.devDependencies ."@mamo
 SDK_SUI_VERSION=$(echo $PACKAGE_CONTENTS | jq --raw-output '.devDependencies ."@mamoru-ai/mamoru-sui-sdk-as"')
 SDK_EVM_VERSION=$(echo $PACKAGE_CONTENTS | jq --raw-output '.devDependencies ."@mamoru-ai/mamoru-evm-sdk-as"')
 SDK_APTOS_VERSION=$(echo $PACKAGE_CONTENTS | jq --raw-output '.devDependencies ."@mamoru-ai/mamoru-aptos-sdk-as"')
+SDK_COSMOS_VERSION=$(echo $PACKAGE_CONTENTS | jq --raw-output '.devDependencies ."@mamoru-ai/mamoru-cosmos-sdk-as"')
 
 # echo $PACKAGE_CONTENTS
 echo $SDK_VERSION
 echo $SDK_SUI_VERSION
 echo $SDK_EVM_VERSION
 echo $SDK_APTOS_VERSION
+echo $SDK_COSMOS_VERSION
 
 # create json file with dependency map
-echo "export const sdkVersions = {\"sdk\": \"$SDK_VERSION\", \"sui\": \"$SDK_SUI_VERSION\", \"evm\": \"$SDK_EVM_VERSION\", \"aptos\": \"$SDK_APTOS_VERSION\"}" > src/sdk-dependency-versions.ts
+echo "export const sdkVersions = {\"sdk\": \"$SDK_VERSION\", \"sui\": \"$SDK_SUI_VERSION\", \"evm\": \"$SDK_EVM_VERSION\", \"aptos\": \"$SDK_APTOS_VERSION\",\"cosmos\": \"$SDK_COSMOS_VERSION\" }" > src/sdk-dependency-versions.ts
--- src/sdk-dependency-versions.ts
+++ src/sdk-dependency-versions.ts
@@ -1,1 +1,1 @@
-export const sdkVersions = {"sdk": "^0.8.0", "sui": "^0.5.0", "evm": "^0.5.2", "cosmos": "^0.5.3", "aptos": "^0.3.3"}
+export const sdkVersions = {"sdk": "^0.8.0", "sui": "^0.5.0", "evm": "^0.5.2", "aptos": "^0.3.3","cosmos": "^0.5.3" }
